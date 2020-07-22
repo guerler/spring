db96b1c0c53a00a0eb52f8e826d240a26739750e
guerler <aysam.guerler@gmail.com>
2020-07-22 14:14:58 -0400

    Add a3m output

--- analyze/0_hhsearchall.sh
+++ analyze/0_hhsearchall.sh
@@ -24,13 +24,12 @@ my @files = glob($path . "*.fasta");
 for my $file(@files) {
 	my $name = fileparse($file, '\..*');
 	my $output = $path . $name . ".hhr";
-	my $hhm = $path . $name . ".hhm";
 	print("\nProcessing: " . $name . "\n");
 	if (-e $output) {
         print("Already available.\n");
         next;
     }
-	my $command = "$binary/hhsearch -i $file -d $db -ohhm $hhm &";
+	my $command = "$binary/hhsearch -i $file -d $db -ohhm $path$name.hhm -oa3m $path$name.a3m &";
 	print ("Executing: $command\n");
 	print "Press ENTER to continue or CTRL-C to exit.";
 	<STDIN>;
