d12c32a45bcd441307e632fca6d9af7d60289d44
guerler <aysam.guerler@gmail.com>
2020-07-23 17:43:53 -0400

    Add hhsearch wrapper

--- analyze/hhsearch_all.py
+++ analyze/hhsearch_all.py
@@ -1,6 +1,10 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
+from multiprocessing import Pool
 import argparse
 import os
+import subprocess
+
+NPROCESS = 3
 
 def main(args):
 	names = []
@@ -8,11 +12,10 @@ def main(args):
 		for index, line in enumerate(file):
 			names.append(line.strip())
 	print ("Loaded %s names from `%s`." % (len(names), args.list))
+	commands = []
 	for name in names:
-		print("\nProcessing: %s" % name)
-		print(30 * "-")
 		input_directory = args.inputs.rstrip("/")
-		sub_directory = name[:len(name)/2]			
+		sub_directory = name[:int(len(name)/2)]			
 		input_file = "%s/fasta/%s/%s.fasta" % (input_directory, sub_directory, name)
 		output_path = "%s/hhr/%s" % (input_directory, sub_directory)
 		if not os.path.exists(output_path):
@@ -21,12 +24,19 @@ def main(args):
 		if os.path.isfile(output_file):
 			print("Already available.")
 			continue
-		background_flag = "" if args.fg else "&";
-		command = "%s -i %s -d %s -ohhr %s %s" % (args.binary, input_file, output_file, args.database, background_flag)
-		print ("Executing: %s" % command)
-		if not args.enter:
-			raw_input('Press ENTER to continue or CTRL-C to exit.')
+		command = "%s -i %s -d %s -o %s" % (args.binary, input_file, args.database, output_file)
 		os.system(command)
+		#command = "echo '%s' >> /Users/guerler/spring/analyze/out.txt" % name
+		#if not args.enter:
+		#	raw_input('Press ENTER to continue or CTRL-C to exit.')
+		#commands.append(command)
+	#with Pool(NPROCESS) as p:
+	#	p.map(execute, commands)
+	#p.close()
+
+def execute(command):
+	print ("Executing: %s" % command)
+	#subprocess.run(command)
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='This script performs HH-search on all matched files in the directory.')
@@ -35,6 +45,5 @@ if __name__ == "__main__":
 	parser.add_argument('-b', '--binary', help='HH-search/HH-blits binary path', required=True)
 	parser.add_argument('-d', '--database', help='HH-search database path', required=True)
 	parser.add_argument('--enter', help='Do not ask user to confirm each command.', action='store_true')
-	parser.add_argument('--fg', help='Disable background submission.', action='store_true')
 	args = parser.parse_args()
 	main(args)
