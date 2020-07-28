d1159e56ed830669993cfb4f94312a030f6e3c6b
guerler <aysam.guerler@gmail.com>
2020-07-28 05:26:10 -0400

    Remove unused parts

--- analyze/hhsearch_all.py
+++ analyze/hhsearch_all.py
@@ -1,10 +1,7 @@
 #! /usr/bin/env python3
-from multiprocessing import Pool
 import argparse
 import os
 import subprocess
-
-NPROCESS = 3
 
 def main(args):
 	names = []
@@ -26,17 +23,6 @@ def main(args):
 			continue
 		command = "%s -i %s -d %s -o %s" % (args.binary, input_file, args.database, output_file)
 		os.system(command)
-		#command = "echo '%s' >> /Users/guerler/spring/analyze/out.txt" % name
-		#if not args.enter:
-		#	raw_input('Press ENTER to continue or CTRL-C to exit.')
-		#commands.append(command)
-	#with Pool(NPROCESS) as p:
-	#	p.map(execute, commands)
-	#p.close()
-
-def execute(command):
-	print ("Executing: %s" % command)
-	#subprocess.run(command)
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='This script performs HH-search on all matched files in the directory.')
@@ -44,6 +30,5 @@ if __name__ == "__main__":
 	parser.add_argument('-i', '--inputs', help='Directory containing `fasta/X/Y.fasta` files', required=True)
 	parser.add_argument('-b', '--binary', help='HH-search/HH-blits binary path', required=True)
 	parser.add_argument('-d', '--database', help='HH-search database path', required=True)
-	parser.add_argument('--enter', help='Do not ask user to confirm each command.', action='store_true')
 	args = parser.parse_args()
 	main(args)
