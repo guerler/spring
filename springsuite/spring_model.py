225c0037a506ca9821cb304a7f1b17f55f8658de
guerler <aysam.guerler@gmail.com>
2020-10-25 14:58:56 -0400

    Write model

--- springsuite/spring_model.py
+++ springsuite/spring_model.py
@@ -4,7 +4,7 @@ import os
 
 class Molecule:
 	def __init__(self, fileName):
-		self.calpha = []
+		self.calpha = {}
 		with open(fileName) as file:
 			for index, line in enumerate(file):
 				if "CA" == line[13:15]:
@@ -14,10 +14,9 @@ class Molecule:
 					occupancy = self.toFloat(line[54:60])
 					temperature = self.toFloat(line[54:60])
 					residue = line[17:20]
-					self.calpha.append(dict(x=x, y=y, z=z,
-											residue=residue,
-											occupancy=occupancy,
-											temperature=temperature))
+					residueNumber = self.toInt(line[22:26])
+					self.calpha[residueNumber] = dict(x=x, y=y, z=z,
+						residue=residue, occupancy=occupancy, temperature=temperature)
 
 	def toFloat(self, x):
 		try:
@@ -25,14 +24,29 @@ class Molecule:
 		except:
 			return 0.0
 
+	def toInt(self, x):
+		try:
+			return int(x)
+		except:
+			return 0
+
+	def save(self, outputName):
+		f = open(outputName, "w")
+		for residueNumber in sorted(self.calpha.keys()):  
+			ca = self.calpha[residueNumber]
+			if ca["residue"] is not None:
+				atom = "ATOM  %5d  CA  %s A %3d    %8.3f%8.3f%8.3f%6.2f%6.2f           C\n" % (residueNumber, ca["residue"], residueNumber, ca["x"], ca["y"], ca["z"], ca["occupancy"], ca["temperature"])
+				f.write(atom)
+		f.close()
+
 class Alignment:
 	def __init__(self, fileName):
 		self.queryName = None
 		self.queryAlignment = []
-		self.queryAlignmentStart = []
+		self.queryStart = []
 		self.templateName = None
 		self.templateAlignment = []
-		self.templateAlignmentStart = []
+		self.templateStart = []
 		self.readFile(fileName)
 
 	def readFile(self, fileName):
@@ -46,20 +60,42 @@ class Alignment:
 				if self.queryName and self.templateName:
 					if len(cols) > 3:
 						if cols[0] == "Q" and cols[1] == self.queryName:
-							self.queryAlignmentStart.append(cols[2])
+							self.queryStart.append(self.toInt(cols[2]))
 							self.queryAlignment.append(cols[3])
 						if cols[0] == "T" and cols[1] == self.templateName:
-							self.templateAlignmentStart.append(cols[2])
+							self.templateStart.append(self.toInt(cols[2]))
 							self.templateAlignment.append(cols[3])
 				if len(cols) > 1 and cols[0] == "No" and cols[1] == "2":
 					break
 
+	def toInt(self, x):
+		try:
+			return int(x)
+		except:
+			return 0
+
 def main(args):
-	# load template molecule
 	templateMolecule = Molecule(args.template)
-	print (templateMolecule.calpha)
 	alignment = Alignment(args.query)
-	print ('\n'.join(alignment.templateAlignment))
+	for residueNumber in templateMolecule.calpha:
+		templateMolecule.calpha[residueNumber]["residue"] = None
+	for i in range(len(alignment.templateStart)):
+		start = alignment.templateStart[i]
+		querySequence = alignment.queryAlignment[i]
+		templateSequence = alignment.templateAlignment[i]
+		n = len(querySequence)
+		tcount = 0
+		for j in range(n):
+			qs = querySequence[j]
+			ts = templateSequence[j]
+			if qs != "-" and ts != "-":
+				residueNumber = start + tcount
+				if residueNumber in templateMolecule.calpha:
+					templateMolecule.calpha[residueNumber]["residue"] = toThreeAmino(qs)
+			if ts != "-":
+				tcount += 1
+	print (templateMolecule.calpha)
+	templateMolecule.save("test.pdb")
 
 def toThreeAmino (seq):
 	code = dict(G="GLY", A="ALA", V="VAL", L="LEU", I="ILE", M="MET", F="PHE", P="PRO", Y="TYR", W="TRP",
