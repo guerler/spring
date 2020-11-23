ae9927ebfc06255ba69c1b06de85b3a012d7f525
guerler <aysam.guerler@gmail.com>
2020-11-23 11:53:15 +0100

    Fix lint

--- utilities/spring_merge.py
+++ utilities/spring_merge.py
@@ -5,7 +5,8 @@ from os.path import isfile, join, getsize
 
 
 def main(args):
-    names = sorted([f for f in listdir(args.path) if isfile(join(args.path, f))])
+    names = sorted([f for f in listdir(args.path)
+                    if isfile(join(args.path, f))])
     files = [join(args.path, name) for name in names]
     sizes = [getsize(f) for f in files]
     start = 0
@@ -21,7 +22,7 @@ def main(args):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Creates single file database')
+    parser = argparse.ArgumentParser(description='Merges files.')
     parser.add_argument('-p', '--path', help='Path to files',
                         required=True)
     parser.add_argument('-x', '--index', help='Output Database Index',
