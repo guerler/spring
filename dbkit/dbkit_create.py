3632646bec5edbe47e06c894e32bfd215b895555
guerler <aysam.guerler@gmail.com>
2020-11-25 16:11:43 +0100

    Avoid storing downloaded pdbs in dbkit database creation

--- dbkit/dbkit_create.py
+++ dbkit/dbkit_create.py
@@ -1,44 +1,11 @@
 #! /usr/bin/env python3
 import argparse
-from os import listdir, system
-from os.path import isfile, join, getsize
+from os import system
+from os.path import getsize
 
 
-def createIndex(inputPath, outputIndex, outputDatabase):
-    names = sorted([f for f in listdir(inputPath) if isfile(join(inputPath, f))])
-    files = [join(args.path, name) for name in names]
-    sizes = [getsize(f) for f in files]
-    start = 0
-    system("rm -f %s" % outputDatabase)
-    with open(outputIndex, 'w') as output_file:
-        for i in range(len(names)):
-            name = names[i]
-            size = sizes[i]
-            file = files[i]
-            output_file.write("%s\t%d\t%d\n" % (name, start, size))
-            start = start + size + 1
-            system("cat %s >> %s" % (file, outputDatabase))
-
-
-def downloadFiles(entries, sourceUrl, outputPath, logFile):
-    outputPath = outputPath.rstrip("/")
-    system("mkdir -p %s" % outputPath)
-    for entryId in entries:
-        logFile.write("Loading %s.\n" % entryId)
-        fileName = "%s/%s" % (outputPath, entryId)
-        if isfile(fileName):
-            logFile.write("Skipping %s.\n" % entryId)
-            continue
-        system("wget -q -O %s %s%s" % (fileName, sourceUrl, entryId))
-        if getsize(fileName) == 0:
-            logFile.write("Entry not found.\n")
-            system("rm %s" % fileName)
-        else:
-            logFile.write("Completed.\n")
-
-
-def getIdentifiers(args, logFile):
-    entries = list()
+def getIdentifiers(args):
+    entries = set()
     with open(args.list) as file:
         for line in file:
             entry = line.split()[0]
@@ -51,16 +18,33 @@ def getIdentifiers(args, logFile):
                 entry = entry.upper()
             if args.idextension is not None:
                 entry = "%s.%s" % (entry, args.idextension)
-            entries.append(entry)
-    logFile.write("Found %s entries from `%s`.\n" % (len(entries), entries))
+            entries.add(entry)
     return entries
 
 
 def main(args):
+    entries = getIdentifiers(args)
     logFile = open(args.log, "w")
-    entries = getIdentifiers(args, logFile=logFile)
-    downloadFiles(entries, args.url, args.path, logFile=logFile)
-    createIndex(args.path, args.index, args.database)
+    logFile.write("Found %s entries.\n" % len(entries))
+    outputIndex = args.index
+    outputDatabase = args.database
+    tempPath = args.temp.rstrip("/")
+    tempFile = "%s/temp.pdb" % tempPath
+    system("mkdir -p %s" % tempPath)
+    system("rm -f %s" % outputDatabase)
+    indexFile = open(outputIndex, 'w')
+    start = 0
+    for entryId in entries:
+        logFile.write("Loading %s.\n" % entryId)
+        system("wget -q -O %s %s%s" % (tempFile, args.url, entryId))
+        tempSize = getsize(tempFile)
+        if tempSize == 0:
+            logFile.write("Entry `%s` not found.\n" % entryId)
+        else:
+            indexFile.write("%s\t%d\t%d\n" % (entryId, start, tempSize))
+            start = start + tempSize + 1
+            system("cat %s >> %s" % (tempFile, outputDatabase))
+        logFile.flush()
     logFile.close()
 
 
@@ -68,12 +52,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='DBKit - Download and Merge files into a single file.')
     parser.add_argument('-l', '--list', help='List of entries', required=True)
     parser.add_argument('-u', '--url', help='Source Url', required=True)
-    parser.add_argument('-p', '--path', help='Path to files', required=True)
-    parser.add_argument('-x', '--index', help='Output Database Index', required=True)
-    parser.add_argument('-d', '--database', help='Output Database', required=True)
+    parser.add_argument('-t', '--temp', help='temp', required=True)
     parser.add_argument('-il', '--idlength', help='Format Identifier Length (integer)', required=False, default="0")
     parser.add_argument('-ic', '--idcase', help='Format Identifier Case (lower, upper)', required=False, default=None)
     parser.add_argument('-ie', '--idextension', help='Format Identifier Extension', required=False, default=None)
-    parser.add_argument('-o', '--log', help="Log file", required=True)
+    parser.add_argument('-o', '--index', help='Output Database Index', required=True)
+    parser.add_argument('-d', '--database', help='Output Database', required=True)
+    parser.add_argument('-g', '--log', help="Log file", required=True)
     args = parser.parse_args()
     main(args)
