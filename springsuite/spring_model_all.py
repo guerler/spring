6c32ee27a41d846dcf7ec21cc5f5504c9ac31e38
guerler <aysam.guerler@gmail.com>
2020-12-16 20:24:28 +0100

    Add spring modeller all module

--- springsuite/spring_model_all.py
+++ springsuite/spring_model_all.py
@@ -1,30 +1,61 @@
 #! /usr/bin/env python3
 import argparse
+from os import mkdir
+from os.path import isdir
+
+from spring_package.DBKit import DBKit
 from spring_package.Modeller import createModel
 
 
+class ModelArguments:
+    def __init__(self, args):
+        self.log = args.log
+        self.index = args.index
+        self.database = args.database
+        self.cross = args.cross
+
+    def set(self, a, b, output):
+        self.a = a
+        self.b = b
+        self.output = output
+
+
 def main(args):
-    hhrPath = args.hhr_path.rstrip("/")
+    modelArgs = ModelArguments()
     outPath = args.output_path.rstrip("/")
-    system("mkdir -p %s" % outPath)
+    if not isdir(outPath):
+        mkdir(outPath)
+    if not isdir("temp"):
+        mkdir("temp")
+    aFile = "temp/a.hhr"
+    bFile = "temp/b.hhr"
+    dbkit = DBKit(args.hhr_index, args.hhr_database)
     with open(args.pairs, "r") as file:
         for line in file:
             param = line.split()
-            a = "%s/%s" % (hhrPath, param[0])
-            b = "%s/%s" % (hhrPath, param[1])
-            outfile = "%s/%s.%s.pdb" % (outPath, param[0], param[1])
-            logfile = "%s/log.txt" % outPath
-            cmdString = "./spring_model.py -a '%s' -b '%s' -c '%s' -i '%s' -d '%s' -o '%s' -g '%s'" % (a, b, args.cross, args.index, args.database, outfile, logfile)
-            system(cmdString)
+            aIdentifier = param[0]
+            bIdentifier = param[1]
+            success = dbkit.createFile(aIdentifier, aFile)
+            if not success:
+                print("Failed to retrieve entry %s." % aIdentifier)
+                continue
+            success = dbkit.createFile(bIdentifier, bFile)
+            if not success:
+                print("Failed to retrieve entry %s." % bIdentifier)
+                continue
+            output = "%s/%s.%s.pdb" % (outPath, aIdentifier, bIdentifier)
+            modelArgs.set(a=aFile, b=bFile, output=output)
+            createModel(modelArgs)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Create 3D models from HH-search results.')
-    parser.add_argument('-p', '--pairs', help='Result table from min-Z evaluation', required=True)
-    parser.add_argument('-hp', '--hhr_path', help='Path to HHR files', required=True)
-    parser.add_argument('-op', '--output_path', help='Output directory', required=True)
+    parser.add_argument('-p', '--pairs', help='Interaction table e.g. from min-Z evaluation (2-columns)', required=True)
+    parser.add_argument('-ih', '--hhr_index', help='HHR Index database file (ffindex)', required=True)
+    parser.add_argument('-dh', '--hhr_database', help='HHR Database file (ffdata)', required=True)
     parser.add_argument('-i', '--index', help='PDB Database Index file (ffindex)', required=True)
-    parser.add_argument('-d', '--database', help='PDB Database files (ffdata)', required=True)
+    parser.add_argument('-d', '--database', help='PDB Database file (ffdata)', required=True)
     parser.add_argument('-c', '--cross', help='PDB Cross Reference', required=True)
+    parser.add_argument('-g', '--log', help='Log file', required=True)
     args = parser.parse_args()
     main(args)
